972482d6794af5115ff6821b08caff4f8a59b76b
Murat Enes Bayar <murat.bayar@8bitiz.com>
2026-02-13 23:14:09 +0300

    Navbar button tasarimini duzenle

--- js/home.js
+++ js/home.js
@@ -1,18 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     const startBtn = document.getElementById("startBtn");
-    const howBtn = document.getElementById("howBtn");
-
+    
     if(startBtn){
         startBtn.addEventListener("click", function(){
             window.location.href = "hesapla.html";
         });
     }
-
-    if(howBtn){
-        howBtn.addEventListener("click", function(){
-            alert("Yakıt, sigorta, MTV ve diğer giderleri girerek aracın yıllık ve aylık maliyetini öğrenebilirsin.");
-        });
-    }
-
 });
